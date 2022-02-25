9330e6f65f77a8e58bcef45fab6d544871bbf8ca
dach83 <dach83@mail.ru>
2022-02-25 16:30:00 +0300

    add MovieAdapter for RecyclerView

---
+++ app/src/main/java/com/example/gb04_android_on_kotlin_movie_finder/ui/adapter/MovieAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.gb04_android_on_kotlin_movie_finder.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gb04_android_on_kotlin_movie_finder.R
+import com.example.gb04_android_on_kotlin_movie_finder.domain.movie.Movie
+
+class MovieAdapter : ListAdapter<Movie, MovieAdapter.ViewHolder>(MovieDiff()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_poster, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(movie: Movie) {
+            // TODO
+        }
+    }
+}
+
+private class MovieDiff : DiffUtil.ItemCallback<Movie>() {
+    override fun areItemsTheSame(oldItem: Movie, newItem: Movie) = oldItem.id == newItem.id
+    override fun areContentsTheSame(oldItem: Movie, newItem: Movie) = oldItem == newItem
+}
--- app/src/main/java/com/example/gb04_android_on_kotlin_movie_finder/ui/movies/MoviesViewModel.kt
+++ app/src/main/java/com/example/gb04_android_on_kotlin_movie_finder/ui/movies/MoviesViewModel.kt
@@ -1,7 +1,25 @@
 package com.example.gb04_android_on_kotlin_movie_finder.ui.movies
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.gb04_android_on_kotlin_movie_finder.data.RepositoryImpl
+import com.example.gb04_android_on_kotlin_movie_finder.domain.movie.Movie
+import com.example.gb04_android_on_kotlin_movie_finder.domain.movie.MovieCategory
+import com.example.gb04_android_on_kotlin_movie_finder.domain.repository.Repository
 
 class MoviesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val repository: Repository = RepositoryImpl()
+    private val _movies = mutableMapOf<MovieCategory, MutableLiveData<List<Movie>>>()
+
+    init {
+        for(category in MovieCategory.values()) {
+            _movies[category] = MutableLiveData<List<Movie>>()
+            _movies[category]?.value = repository.getMovies(category)
+        }
+    }
+
+    fun getMoviesList(category: MovieCategory): LiveData<List<Movie>> = _movies[category]!!
+
 }
